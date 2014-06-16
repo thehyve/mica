42d059ee1a4bff3f861d5f8f235e4037a128d03a
dwerbam <dwerbam@gmail.com>
2014-06-16 23:03:44 +0200

    try to pick zip from nexus if tgz not exist

--- cmi/vagrant-mica/manifests/mica.pp
+++ cmi/vagrant-mica/manifests/mica.pp
@@ -1,6 +1,26 @@
-include wget
 
+#
+#exec { "wget ":
+#  command => "wget ",
+#  cwd     => "/tmp/mica",
+#  path    => $binpath,
+#  timeout => 0
+#}
 
+exec { "wget from nexus if tgz not exist":
+  command => "wget ... ",
+  cwd     => "/vagrant",
+  creates => 'mica.tar.gz',
+  path    => $binpath,
+  timeout => 0
+} ->
+exec { "uncompress":
+  command => "wget ",
+  cwd     => "/vagrant",
+  creates => 'mica.tar.gz',
+  path    => $binpath,
+  timeout => 0
+} ->
 file { "/var/www/html/mica":
   ensure => directory,
   recurse => true,
