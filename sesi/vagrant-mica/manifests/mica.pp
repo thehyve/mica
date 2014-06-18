ac172fdad217d37cda32489b66bda91c8a107398
dwerbam <dwerbam@gmail.com>
2014-06-18 11:57:20 +0200

    changed to grab jar from nexus

--- sesi/vagrant-mica/manifests/mica.pp
+++ sesi/vagrant-mica/manifests/mica.pp
@@ -7,16 +7,15 @@
 #  timeout => 0
 #}
 $zipname = 'mica_distribution-7.x-9.1-b3211'
-$zipfile = "${zipname}.zip"
+$zipfile = "${zipname}.jar"
 $wwwfolder = '/var/www/html'
 
-exec { "wget from nexus if tgz not exist":
-  command => "wget ... ",
+exec { "wget from nexus if jar not exist":
+  command => "wget https://repo.thehyve.nl/service/local/repositories/releases/content/org/obiba/mica/mica-distribution/7.x-9.1-b3211/mica-distribution-7.x-9.1-b3211.jar ",
   cwd     => "/vagrant",
   creates => "/vagrant/${zipfile}",
   path    => "/usr/local/bin/:/bin/:/usr/bin/",
 } ->
-
 exec { "copy zip to www":
   command => "cp  -f ${zipfile} ${wwwfolder}",
   cwd     => "/vagrant",
