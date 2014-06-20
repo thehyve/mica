6b0bcc147e33f91d08495a8fb294879f710e7468
dwerbam <dwerbam@gmail.com>
2014-06-20 10:09:46 +0200

    added permission to write in shared folder for apache user

--- sesi/vagrant-mica/manifests/devmodules.pp
+++ sesi/vagrant-mica/manifests/devmodules.pp
@@ -4,7 +4,19 @@ exec { "symlink to modules":
   path    => "/usr/local/bin/:/bin/:/usr/bin/",
   creates => '/var/www/html/mica/sites/all/modules/sesi',
   subscribe   => File["/var/www/html/mica"],
+} ->
+exec { "giving access to shared folder for apache user":
+  command => 'usermod -a -G vboxsf apache',
+  path    => "/usr/local/bin/:/bin/:/usr/bin/",
+} ->
+exec { "changing sesimodules permissions":
+  command => 'chmod -R a+rw /sesimodules',
+  path    => "/usr/local/bin/:/bin/:/usr/bin/",
 }
+
+
+
+
 
 
 
