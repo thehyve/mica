d2bac0469c5719d0d93cf523bfdbd117c6a82d07
dwerbam <dwerbam@gmail.com>
2014-06-25 06:43:26 +0200

    added manual puppet install

---
+++ sesi/vagrant-mica/manifests/install.pp
@@ -0,0 +1,10 @@
+### FIREWALL ##########
+resources { "firewall":
+  purge => true
+}
+
+
+import 'lamp.pp'
+import 'tomcat.pp'
+import 'mica.pp'
+import 'micadb.pp'
---
+++ sesi/vagrant-mica/shell/installssl.sh
@@ -0,0 +1,15 @@
+yum install mod_ssl openssl
+
+# Generate private key 
+openssl genrsa -out ca.key 2048 
+
+# Generate CSR 
+openssl req -new -key ca.key -out ca.csr
+
+# Generate Self Signed Key
+openssl x509 -req -days 365 -in ca.csr -signkey ca.key -out ca.crt
+
+# Copy the files to the correct locations
+cp ca.crt /etc/pki/tls/certs
+cp ca.key /etc/pki/tls/private/ca.key
+cp ca.csr /etc/pki/tls/private/ca.csr
