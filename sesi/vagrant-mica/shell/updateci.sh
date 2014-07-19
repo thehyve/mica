9ad66bd13ca92784ce875995a050bda980aac198
root <root@cm01dv-rhweb001.amnl05.cognizantdc.com>
2014-07-19 16:43:02 +0200

    changed CI method

--- sesi/vagrant-mica/shell/updateci.sh
+++ sesi/vagrant-mica/shell/updateci.sh
@@ -3,52 +3,23 @@
 #Any command that fails will cause the entire script to fail and return an exit status 
 set -e
 
-GITSRC=/opt/mica
 MICA=/var/www/html/mica
-SESIMODS=$MICA/sites/all/modules/sesi
-DRUSHDIR=/opt/drush
-COMPOSERDST=/usr/bin/composer
+SESIMODS=$MICA/sites/all
 
-if [ ! -d $GITSRC ]; then
-  echo "Mica repo not found. Cloning!"
-  cd /opt
-  git clone https://github.com/thehyve/mica.git
-fi
+#if [ ! -d $GITSRC ]; then
+#  echo "Mica repo not found. Cloning!"
+#  cd /opt
+#  git clone https://github.com/thehyve/mica.git
+#fi
 
-cd $GITSRC
+cd $SESIMODS
 
 if [ "`git pull`" = "Already up-to-date." ]; then
   echo "Nothing to do"
   exit 0
 fi
 
-#copying files from updated repo
-if [ ! -d $SESIMODS ]; then
-  mkdir $SESIMODS
-fi
-
 #continue with CI defined processes
-echo "Updated from repo"
+echo "Updated from repo and running drupalpull.sh"
+bash drupalpull.sh
 
-#trying to install composer and drush
-if [ ! -f $COMPOSERDST ]; then
-  echo "Installing composer"
-  curl -sS https://getcomposer.org/installer | php
-  mv composer.phar $COMPOSERDST
-fi
-
-if [ ! -d $DRUSHDIR ]; then
-  echo "Drush not found. Installing drush!"
-  cd /opt
-  git clone https://github.com/drush-ops/drush.git
-  cd $DRUSHDIR
-  composer install
-  chmod a+x drush
-  cd /usr/bin
-  ln -s $DRUSHDIR/drush 
-fi
-
-rsync -a --delete $GITSRC/sesi/modules $SESIMODS
-cd $MICA
-drush updatedb
-drush features-revert-all
