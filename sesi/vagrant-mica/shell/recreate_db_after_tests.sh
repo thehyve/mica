2f56d12bd6bec52c595283a237490c7b89c77136
root <root@cm01dv-rhweb001.amnl05.cognizantdc.com>
2014-08-13 13:00:11 +0200

    fixed minor bug

--- sesi/vagrant-mica/shell/recreate_db_after_tests.sh
+++ sesi/vagrant-mica/shell/recreate_db_after_tests.sh
@@ -8,12 +8,12 @@ BAMBOO_PLAN=OC-MIC
 MICA=/var/www/html/mica
 SESIMODS=$MICA/sites/all
 
-cd $SESIMODS
 
 SHD=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+cd $SESIMODS
 
-$SHD/is_there_new_build.py $BAMBOO_SERVER $BAMBOO_PLAN \
+python $SHD/is_there_new_build.py $BAMBOO_SERVER $BAMBOO_PLAN \
 && drush sql-drop --yes \
-; `drush sql-connect` < $1
+&& `drush sql-connect` < $1
 
 exit 0
